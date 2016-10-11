404853f1bb6adb4c232abe06bc89d240184a852f
Mario Haustein <mario.haustein@hrz.tu-chemnitz.de>
2016-10-11 15:39:52 +0200

    LUA-Init-Code für DESFire-Shell

---
+++ luainit.lua
@@ -0,0 +1,24 @@
+function AES(k, v)
+  if not k then k = "00000000000000000000000000000000" end
+  if not v then v = 0 end
+  return { t = "AES", k = k, v = v }
+end
+
+
+function _3K3DES(k, kvno)
+  if not k then k = "000000000000000000000000000000000000000000000000" end
+  if not v then v = 0 end
+  return { t = "3K3DES", k = k, v = v }
+end
+
+function _3DES(k, kvno)
+  if not k then k = "00000000000000000000000000000000" end
+  if not v then v = 0 end
+  return { t = "3DES", k = k, v = v }
+end
+
+function DES(k, kvno)
+  if not k then k = "0000000000000000" end
+  if not v then v = 0 end
+  return { t = "DES", k = k, v = v }
+end
