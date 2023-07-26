c40c9e7f214e1657eb9efbf86821184b6b310052
Mario Haustein <mario.haustein@hrz.tu-chemnitz.de>
2023-07-26 09:17:36 +0200

    Update test cases

--- test/keydiv.lua
+++ test/keydiv.lua
@@ -1,2 +1,7 @@
-print(buf.th(crypto.cmac("AES-128-CBC", "01020202020202023f333355987654020202020202023f333355987654", "00112233445566778899aabbccddeeff")))
-print(buf.th(key.div({ t = "AES", k = "00112233445566778899aabbccddeeff", v = 0 }, "02020202020202", "0x3f3333", "0x55", "987654")["k"]))
+result = buf.th(crypto.cmac("AES-128-CBC", "01020202020202023f333355987654020202020202023f333355987654", "00112233445566778899aabbccddeeff"))
+print(result)
+assert(result == "39d7b23f8b3cc670d249df313968bbce")
+
+result = buf.th(key.div({ t = "AES", k = "00112233445566778899aabbccddeeff", v = 0 }, "02020202020202", "0x3f3333", "0x55", "987654")["k"])
+print(result)
+assert(result == "39d7b23f8b3cc670d249df313968bbce")
